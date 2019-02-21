5556979785c3d97b8ce1f30af419610e452cf53b
26139681 <felipemateus1203@gmail.com>
2019-02-21 20:03:33 -0300

    Exemplo aula 03 / resultado (-1)

--- src/Exemplos/Catalogo.java
+++ src/Exemplos/Catalogo.java
@@ -8,5 +8,27 @@ public class Catalogo {
             a=c;
         }
         System.out.println(a);
+        if (c>a){
+            c=a;
+        }else {
+            c = b;
+        }
+        System.out.println(c);
+        /*switch*/
+        int op=03;
+        int v=0;
+        switch (op) {
+            case 1:
+                v = 1;
+                break;
+            case 2:
+                v = 2;
+                break;
+            default:
+                v = -1;
+        }
+        System.out.println(v);
         }
     }
+
+
