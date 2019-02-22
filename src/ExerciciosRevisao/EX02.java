474fe37de6f06b6f4100665e2da6e4e09a14578c
26139681 <felipemateus1203@gmail.com>
2019-02-21 21:05:22 -0300

    aula 03 / EX02

--- src/ExerciciosRevisao/EX02.java
+++ src/ExerciciosRevisao/EX02.java
@@ -1,4 +1,15 @@
 package ExerciciosRevisao;
 
+import java.util.Scanner;
+
 public class EX02 {
+    public  static  void main(String[] args){
+        int totalFolha=100;
+        Double resultado;
+        if (totalFolha<100){
+            resultado=totalFolha*,25;
+        }else{
+            resultado=totalFolha*0,20;
+        }
+    }
 }
